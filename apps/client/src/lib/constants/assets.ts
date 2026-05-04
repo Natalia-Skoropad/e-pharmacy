05d46d30801d6c57cf6462409ced53c86136075f
Nata <skoropad_natalia@ukr.net>
2026-05-04 15:53:29 +0300

    chore: add client global styles and assets foundation

---
+++ apps/client/src/lib/constants/assets.ts
@@ -0,0 +1,11 @@
+export const ASSETS = {
+  iconsSprite: '/icons/sprite.svg',
+  defaultOgImage: '/og-image.png',
+} as const;
+
+export const IMAGE_DIRS = {
+  common: '/images/common',
+  home: '/images/home',
+  stores: '/images/stores',
+  products: '/images/products',
+} as const;
--- apps/client/src/lib/constants/metadata.ts
+++ apps/client/src/lib/constants/metadata.ts
@@ -1,5 +1,7 @@
 import { APP_NAMES } from '@e-pharmacy/config';
 
+import { ASSETS } from './assets';
+
 export const SITE_NAME = APP_NAMES.client;
 
 export const SITE_URL =
@@ -9,6 +11,6 @@ export const HOME_TITLE = 'Online Pharmacy Store';
 export const HOME_DESCRIPTION =
   'Order medicines online, explore trusted pharmacy stores, and manage your health essentials with E-PHARMACY.';
 
-export const DEFAULT_OG_IMAGE = '/og-image.png';
+export const DEFAULT_OG_IMAGE = ASSETS.defaultOgImage;
 export const DEFAULT_OG_IMAGE_ALT =
   'E-PHARMACY online pharmacy storefront preview';
